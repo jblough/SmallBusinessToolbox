f83ae239d93d190163b84c553f43ff12bc201194
Joe Blough <joe.blough@gmail.com>
2011-11-26 20:24:31 -0500

    Add filtering to license and permit data

--- src/com/josephblough/sbt/adapters/LicenseAndPermitDataAdapter.java
+++ src/com/josephblough/sbt/adapters/LicenseAndPermitDataAdapter.java
@@ -12,11 +12,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LicenseAndPermitDataAdapter extends ArrayAdapter<LicenseAndPermitData> {
+public class LicenseAndPermitDataAdapter extends ArrayAdapter<LicenseAndPermitData> implements Filterable {
 
     private LayoutInflater inflater = null;
     Context context;
